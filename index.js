501a741f84e2c6f0b6fed6623c4b4484a9af2aef
Pronnoy Goswami <pronnoy.goswami@gmail.com>
2016-08-31 02:42:43 +0530

    Name retrieval working with a module.

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 'use strict';
 
-const Hapi = require('hapi');
+const Hapi = require('hapi'),
+    Joi = require('joi'),
+    names = require('./retrieve-name');
 
 const server = new Hapi.Server();
 server.connection({port: 3000});
 
+// let nameConfig = {
+//     handler: function(request, reply) {
+//         let names = request.params.name.split('/');
+//         reply({
+//             first: names[0],
+//             last: names[1],
+//             mood: request.query.mood
+//         });
+//     },
+//     validate: {
+//         params: {
+//             name: Joi.string().min(8).max(100)
+//         },
+//         query: {
+//             mood: Joi.string().valid(["neutral","happy","sad"]).default("neutral")
+//         }
+//     }
+// };
+
+// server.route({
+//     method: 'GET',
+//     path: '/name/{name*2}',
+//     config: nameConfig
+// });
+
+// let names = [
+//     {
+//         id: 1,
+//         name: 'Pronnoy Goswami'
+//     },
+//     {
+//         id: 2,
+//         name: 'Abhinav Dhasmana'
+//     },
+//     {
+//         id: 3,
+//         name: 'Mahesh Haldar',
+//     }
+// ];
+
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/names',
     handler: function (request, reply) {
-        reply('I am awesome!!!');
+        const result = names();
+        reply(result);
     }
 });
 
---
+++ retrieve-name.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function() {
+    let names = [
+        {
+            name: 'Pronnoy Goswami'
+        },
+        {
+            name: 'Abhinav Dhasmana'
+        },
+        {
+            name: 'Mahesh Haldar',
+        }
+    ];
+    return names;
+};
