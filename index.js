802e792bbaacca75d220fff1a50efe276b2bf4e8
Pronnoy Goswami <pronnoy.goswami@gmail.com>
2016-08-31 11:03:57 +0530

    Commented code removed

--- index.js
+++ index.js
@@ -7,46 +7,6 @@ const Hapi = require('hapi'),
 const server = new Hapi.Server();
 server.connection({port: 3000});
 
-// let nameConfig = {
-//     handler: function(request, reply) {
-//         let names = request.params.name.split('/');
-//         reply({
-//             first: names[0],
-//             last: names[1],
-//             mood: request.query.mood
-//         });
-//     },
-//     validate: {
-//         params: {
-//             name: Joi.string().min(8).max(100)
-//         },
-//         query: {
-//             mood: Joi.string().valid(["neutral","happy","sad"]).default("neutral")
-//         }
-//     }
-// };
-
-// server.route({
-//     method: 'GET',
-//     path: '/name/{name*2}',
-//     config: nameConfig
-// });
-
-// let names = [
-//     {
-//         id: 1,
-//         name: 'Pronnoy Goswami'
-//     },
-//     {
-//         id: 2,
-//         name: 'Abhinav Dhasmana'
-//     },
-//     {
-//         id: 3,
-//         name: 'Mahesh Haldar',
-//     }
-// ];
-
 server.route({
     method: 'GET',
     path: '/names',
